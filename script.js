4889d9d4488df58edf147f07895b6384f461207f
Aoife Fogarty <fogarty@mpip-mainz.mpg.de>
2016-06-09 17:58:48 +0200

    get and plot data from two different stations
    make two requests to NOAA
    plot two datasets using d3

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
-var stationNameToId = {};
+//global variables
+var weatherData; //data from both stations from request to NOAA API
+var stationNameToId = {}; //map name to GHCND ID
 var timeFormat = d3.time.format('%Y-%m-%dT%H:%M:%S');
-var temperatureBuffer = 5; //for adding padding inside plot
-
-window.onload = function () {
-  //if (localStorage.get("hasCodeRunBefore") === null) {
-    d3.csv("ghcnd-stations.csv",function(error,data) {
-      console.log(error);
-      for (var i in data) {
-        if ((Math.abs(parseFloat(data[i].latitude) - 50.0) < 2.0) && (Math.abs(parseFloat(data[i].longitude) - 10.0) < 2.0)) {
-          stationNameToId[data[i].locationname.trim()] = data[i].locationid.trim();
-          //console.log(data[i].locationname.trim());
-        }
-      }
-    });
-  //  localStorage.setItem("hasCodeRunBefore", true);
-  //}
-}
-
-var jsonData;
-
-function fahrenheitToCelcius(fahrTemp) {
-  return (fahrTemp - 32) * 5 / 9;
-}
+var yBuffers = { //for adding padding on y axis inside plot
+  "TMAX":5,
+  "TMIN":5,
+  "PRCP":5,
+  "TPCP":5,
+  "MMNT":5,
+  "MMXT":5,
+}; 
 
 //units are for output data, not for data read from NOAA
 //data must be converted
@@ -34,6 +22,26 @@ var mapDataKeysToLabels = {
   "MMXT":"Monthly mean maximum temperature / Celcius", //Fahrenheit on NOAA
 }
 
+window.onload = function () {
+  //if (localStorage.get("hasCodeRunBefore") === null) {
+    d3.csv("../ghcnd-stations.csv",function(error,data) {
+      console.log(error);
+      for (var i in data) {
+        if ((Math.abs(parseFloat(data[i].latitude) - 50.0) < 2.0) && (Math.abs(parseFloat(data[i].longitude) - 10.0) < 2.0)) {
+          stationNameToId[data[i].locationname.trim()] = data[i].locationid.trim();
+          //console.log(data[i].locationname.trim());
+        }
+      }
+    });
+  //  localStorage.setItem("hasCodeRunBefore", true);
+  //}
+}
+
+
+function fahrenheitToCelcius(fahrTemp) {
+  return (fahrTemp - 32) * 5 / 9;
+}
+
  
 function updatePlot() {
   //TODO fire when dropdownData is changed
@@ -41,7 +49,7 @@ function updatePlot() {
   //var string = '';
   //string = 'Plotting '+ dropdownData.value + ' data for stations ' + dropdownCity1.value + ' and ' + dropdownCity2.value;
   //d3.json('backup.json',draw);
-  draw(jsonData.results);
+  draw(weatherData);
   //draw(testData);
 }
 
@@ -55,21 +63,28 @@ function getAPIKey() {
 
 function requestData() {
   var APIkey = getAPIKey();
+  //TODO get url from dropdownCity1.value and dropdownCity2.value
+  url1 = baseUrl + stationNameToId["KASSEL"] + dateUrl + typeUrl;
+  url2 = baseUrl + stationNameToId["MANNHEIM"] + dateUrl + typeUrl;
+  console.log(url1);
+  console.log(url2);
+  responseData1 = makeHttpRequest(url1,APIkey);
+  responseData2 = makeHttpRequest(url2,APIkey);
+  weatherData = {"station1":responseData1,"station2":responseData2};
+}
+
+function makeHttpRequest(url,APIkey) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
-      document.getElementById("demo").innerHTML = xhttp.responseText;
-      jsonData = JSON.parse(xhttp.responseText);
-      //console.log(jsonData.results[0]);
-      //console.log(jsonData.results[1]);
+      //document.getElementById("demo").innerHTML = xhttp.responseText;
+      responseData = JSON.parse(xhttp.responseText);
     }
   };
-  url = baseUrl + stationNameToId["KASSEL"] + dateUrl + typeUrl;
-  console.log(url);
-  //get url from dropdownCity1.value and dropdownCity2.value
   xhttp.open("GET", url, false);
   xhttp.setRequestHeader("token",APIkey.trim());
   xhttp.send();
+  return responseData;
 }
 
 function draw(data) {
@@ -89,19 +104,37 @@ function draw(data) {
     .append("svg")
       .attr("width",width)
       .attr("height",height)
-    .selectAll("circle")
-    .data(data)
+    .append("g")
+      .attr("class","chart");
+
+  d3.select("svg")
+    .selectAll("circle.station1")
+    .data(data.station1.results)
     .enter()
     .append("circle");
+      .attr("class","station1");
 
-  //TODO make extent bigger as a function of which data is plotted
-  var yBuffer = temperatureBuffer;
-  var xExtent = d3.extent(data, function(d){return timeFormat.parse(d.date)});
+  d3.select("svg")
+    .selectAll("circle.station2")
+    .data(data.station2.results)
+    .enter()
+    .append("circle");
+      .attr("class","station2");
+
+  var xExtent = d3.extent(
+    data.station1.results.concat(data.station2.results),
+    function(d){return timeFormat.parse(d.date)}
+  );
   //var xExtent = [new Date(2000, 3, 1),new Date(2001, 6, 1)]
   var xScale = d3.time.scale()
       .range([margin,width-margin])
       .domain(xExtent);
-  var yExtent = d3.extent(data, function(d){return d.value});
+
+  var yBuffer = yBuffers[dataKey];
+  var yExtent = d3.extent(
+    data.station1.results.concat(data.station2.results),
+    function(d){return d.value}
+  );
   var yScale = d3.scale.linear()
       .range([height-margin,margin])
       .domain([yExtent[0]-yBuffer,yExtent[1]+yBuffer]);
@@ -115,10 +148,15 @@ function draw(data) {
   //lines
   var line = d3.svg.line()
       .x(function(d){return xScale(timeFormat.parse(d.date))})
-      .y(function(d){return yScale(d.value)})
+      .y(function(d){return yScale(d.value)});
   d3.select("svg")
     .append("path")
-      .attr("d", line(data))
+      .attr("d", line(data.station1.results))
+      .attr("class", "station1");
+  d3.select("svg")
+    .append("path")
+      .attr("d", line(data.station2.results))
+      .attr("class", "station2");
  
   //axes
   var xAxis = d3.svg.axis().scale(xScale);
@@ -156,7 +194,7 @@ var endpoint = 'stations'
 //var url = 'http://www.ncdc.noaa.gov/cdo-web/api/v2/stations?limit=1000'
 //var url = 'http://www.ncdc.noaa.gov/cdo-web/api/v2/stations?extent=50,8,52,10'
 //var url = 'http://www.ncdc.noaa.gov/cdo-web/api/v2/stations/GHCND:GME00102276'
-var url = 'http://www.ncdc.noaa.gov/cdo-web/api/v2/datasets?stationid=GHCND:GME00102276'
+//var url = 'http://www.ncdc.noaa.gov/cdo-web/api/v2/datasets?stationid=GHCND:GME00102276'
 //var url = 'http://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND&stationid=GHCND:GME00102276&startdate=2010-05-01&enddate=2010-05-01'
 //var url = 'http://www.ncdc.noaa.gov/cdo-web/api/v2/datasets?stationid=GHCND:GME00102276'
 
